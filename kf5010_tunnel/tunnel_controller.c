1643e2a4bd31821c801a9a2cec23ad01edc7cccc
Oumar Ly <lyomrau@gmail.com>
2017-12-05 11:12:06 +0000

    add tunnel direction and delay

--- kf5010_tunnel/tunnel_controller.c
+++ kf5010_tunnel/tunnel_controller.c
@@ -1,20 +1,55 @@
 #include <assert.h>
 #include "tunnel_controller.h"
 #include "console.h"
+#include <semaphore.h>
 
-static int n_in_tunnel[2];
+static int n_in_tunnel[1];
+static int s_in_tunnel[2];
 static unsigned long n_total[2];
+
+
+
+void random_delay(void) {
+    int delay = rand() % 100;
+    int i;
+
+    for (i = 0; i < delay; i+=1) {
+        /* skip */
+    }
+}
+
 
 /**
  * @brief controls entry to the tunnel
  * @param dir the direction of travel when entering
  */
 void enter_tunnel(direction_t dir) {
-    n_in_tunnel[dir] += 1;
+    n_in_tunnel[1] += 1;
 
-    lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
-            (dir == NORTH) ? "NORTH" : "SOUTH", n_in_tunnel[dir]);
-    /*assert(n_in_tunnel[opposite(dir)] == 0);*/
+    //lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
+      //      (dir == NORTH) ? "NORTH" : "SOUTH", n_in_tunnel[dir]);
+    //assert(n_in_tunnel[opposite(dir)] == 0);
+    
+    int rc;
+
+    while (true) {
+        rc = sem_wait(&s_in_tunnel[2]);
+        assert(rc == 0);
+        rc = sem_wait(n_total[2]);
+        assert(rc == 0);
+
+        s_in_tunnel[2] += 1;;
+        random_delay();
+        n_total[2] += 1;;
+        //assert((count1 + count2) == total);
+        lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
+            (dir == NORTH) ? "NORTH" : "SOUTH", s_in_tunnel[2]);
+
+        rc = sem_post(s_in_tunnel[2]);
+        assert(rc == 0);
+        rc = sem_post(&n_in_tunnel[1]);
+        assert(rc == 0);
+    }
 }
 
 /**
@@ -22,7 +57,7 @@ void enter_tunnel(direction_t dir) {
  * @param dir the direction of travel when leaving
  */
 void exit_tunnel(direction_t dir) {
-    n_in_tunnel[dir] -= 1;
+    n_in_tunnel[2] -= 1;
     n_total[dir] += 1;
     lcd_write_at(4+dir, 0, "In tunnel (%s) : %2d",
             (dir == NORTH) ? "NORTH" : "SOUTH", n_in_tunnel[dir]);
